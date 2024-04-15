066c32b45ee372373cc4f10c8ee5a2466aa6cc13
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2024-04-15 16:03:47 -0700

    changing colors and adding effects

--- script.js
+++ script.js
@@ -19,3 +19,52 @@ function rotateImages() {
 
 // Interval for the rotation
 let rotationInterval = setInterval(rotateImages, 2000); 
+
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('.footer');
+    var footerPosition = footer.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        footer.classList.add('show');
+    } else {
+        footer.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('.section3');
+    var footerPosition = footer.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        footer.classList.add('show');
+    } else {
+        footer.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('.section4');
+    var footerPosition = footer.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        footer.classList.add('show');
+    } else {
+        footer.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('#resume');
+    var footerPosition = footer.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        footer.classList.add('show');
+    } else {
+        footer.classList.remove('show');
+    }
+});
